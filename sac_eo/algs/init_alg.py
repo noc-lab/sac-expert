37f21567ee0a0fb845471b55207265caaaf00429
erhancanozcan <43606196+erhancanozcan@users.noreply.github.com>
2024-08-11 14:12:51 -0400

    Fix import error

--- sac_eo/algs/init_alg.py
+++ sac_eo/algs/init_alg.py
@@ -1,4 +1,4 @@
-from sac_eo.algs.mfrl_onpolicy_alg import MFRLOnPolicyAlg
+#from sac_eo.algs.mfrl_onpolicy_alg import MFRLOnPolicyAlg
 from sac_eo.algs.mbrl_onpolicy_alg import MBRLOnPolicyAlg
 from sac_eo.algs.SAC import SAC
 from sac_eo.algs.SAC_expert import SAC_exp
@@ -12,10 +12,10 @@ def init_alg(idx,env,env_eval,env_expert,actor,critics,q_targets,q_critics,model
 
     alg_type = alg_kwargs['alg_type']
 
-    if alg_type == 'mfrl':
-        alg = MFRLOnPolicyAlg(idx,env,env_eval,actor,critics,alg_kwargs,
-            mf_update_kwargs)
-    elif alg_type == 'mbrl':
+    #if alg_type == 'mfrl':
+    #    alg = MFRLOnPolicyAlg(idx,env,env_eval,actor,critics,alg_kwargs,
+    #        mf_update_kwargs)
+    if alg_type == 'mbrl':
         alg = MBRLOnPolicyAlg(idx,env,env_eval,actor,critics,models,alg_kwargs,
             mf_update_kwargs)
     elif alg_type == 'sac':
